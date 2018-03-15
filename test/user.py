93d2f8c61a1a6365bffd8d41ae4b06759b2b2cbc
Joe Smith <ckhosra1@yahoo.com>
2018-03-15 08:45:33 -0700

    DCP #7 Changed check for DoesNotExist to None
    Updated to handle new None return value for non-existent users (instead
    of DoesNotExist)

--- test/user.py
+++ test/user.py
@@ -1,34 +1,64 @@
 import test_setup
 import unittest
 
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import authenticate, get_user_model, get_user
 
 class testUser(unittest.TestCase):
     def testUser(self):
+        
+        USERNAME = 'oroku'
+        EMAILADDRESS = 'oroku@saki.com'
+        PASSWORD = 'wowzers'
+        
         # Create new user
         newuser = get_user_model().objects.create_user(
-            password = 'adminadmin', 
+            password = PASSWORD, 
             usertype = 'ST', 
             firstname = 'Test', 
             lastname = 'Orama',
-            username = 'buffgecko',
-            emailaddress = 'joe@smith.com'
+            username = USERNAME,
+            emailaddress = EMAILADDRESS
         )
         
         self.assertEqual(newuser.firstname,'Test')
         
         # Get an existing user
-        try:
-            newuser2 = get_user_model().objects.get(newuser.userid)
-            print(newuser2)
-        except get_user_model().DoesNotExist:
-            print("No user found")
-        
-        auth_check = authenticate(username='joe@smith.com',password='adminadmin')
-        print("Authenticating...", end="")
-        self.assertTrue(auth_check)
+        newuserget = get_user_model().objects.get(newuser.userid)
+        self.assertIsNotNone(newuserget) # positive
+        self.assertIsNone(get_user_model().objects.get(10000)) # negative
 
-        if(auth_check):
-            print (auth_check.is_authenticated)
-        else:
-            print(False)
+        # Authenticate
+        self.assertTrue(authenticate(username=USERNAME,password=PASSWORD)) # positive
+        self.assertFalse(authenticate(username=USERNAME,password='wrongpass')) # negative
+        self.assertFalse(authenticate(username='wronguser',password=USERNAME)) # negative
+
+        # Get all users
+        allusers = get_user_model().objects.all()
+         
+        for myuser in allusers:
+            print (myuser.username)
+ 
+        # Authentication user lookup
+        self.assertTrue(get_user_model().objects.get_user_auth(None, EMAILADDRESS)) # positive
+        self.assertTrue(get_user_model().objects.get_user_auth(USERNAME, None)) # positive
+        self.assertFalse(get_user_model().objects.get_user_auth(None, 'wrongemail')) # negative
+        self.assertFalse(get_user_model().objects.get_user_auth('elsha', None)) # negative
+
+        # Save to DB
+        newuserget.firstname = 'New first name'
+        newuserget.save()
+        newusergetget = get_user_model().objects.get(newuserget.userid)
+        
+        self.assertEqual(newusergetget.firstname, 'New first name')
+
+        # Admin checks
+        self.assertIsNotNone(newusergetget.is_admin())
+        self.assertIsNotNone(newusergetget.is_superuser())
+
+        # Delete user
+        newusergetget.delete()
+        
+        self.assertIsNone(get_user_model().objects.get(newusergetget.userid))
+
+if __name__ == '__main__':
+    unittest.main() # Run all tests
--- users/backends.py
+++ users/backends.py
@@ -1,21 +1,23 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+
 class MyBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None):
 
+        # Determine login type (e-mail vs. username)
         if '@' in str(username):
             kwargs = {'emailaddress': username}
         else:
             kwargs = {'username': username}
-        try:
-            user = get_user_model().objects.get_user_auth(**kwargs)
-            if user.check_password(password):
-                return user
-        except get_user_model().DoesNotExist:
+
+        # Lookup user
+        user = get_user_model().objects.get_user_auth(**kwargs)
+        
+        # Check for valid password (if user exists)
+        if (user and user.check_password(password)):
+            return user
+        else:
             return None
         
     def get_user(self, userid):
-        try:
-            return get_user_model().objects.get(userid=userid)
-        except get_user_model().DoesNotExist:
-            return None
+        return get_user_model().objects.get(userid=userid)
