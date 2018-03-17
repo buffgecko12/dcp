dac95dfd7707f30ac3a5c811f6601a88549885f8
Chris Khosravi <ckhosra1@yahoo.com>
2018-03-17 10:12:28 -0700

    .

--- test/test_contract.py
+++ test/test_contract.py
@@ -7,7 +7,7 @@ from wakemeup.models.contracts import Contract
 from wakemeup.models.environment import School, Class, Teacher
 
 import json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from psycopg2.extras import DateTimeTZRange
 
@@ -87,8 +87,8 @@ class testContract(unittest.TestCase):
             newteacher.teacheruserid, 
             DateTimeTZRange(datetime(2015, 1, 1, 0, 0, 0), datetime(2016, 1, 1, 0, 0, 0)), # Contract Valid Period
             False, # Guardian approval flag
-            datetime.now(), # Revision deadline ts
-            'Some changes to the original contract', # Revision description
+            datetime.now() + timedelta(days=10), # Revision deadline ts
+            None, # Revision description
             None, # Revision approval TS
             'Don''t do dees!', # Student leader reqs
             'Great teacher reqs', # Teacher reqs 
@@ -101,6 +101,20 @@ class testContract(unittest.TestCase):
             )
 
         newcontract.contractid = newcontract.save()
+
+        # Approve contract
+        newcontract.approve(1,'C',None,datetime.now(),1)
+        newcontract.approve(2,'C',None,datetime.now(),1)
+        newcontract.approve(3,'C',None,datetime.now(),1)
+        newcontract.approve(4,'C',None,datetime.now(),1)
+
+        # Revise contract
+        newcontract.revisiondescription = 'My revision description mang!'
+        newcontract.revise()
+
+        # Approve revision
+        newcontract.approve(1,'R',None,datetime.now(),1)
+        newcontract.approve(2,'R',None,datetime.now(),1)
 
     @classmethod
     def tearDownClass(self):
