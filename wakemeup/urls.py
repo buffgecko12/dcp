6c19b19d2e0c56bed3c326ce28eee6fad5dab661
Joe Smith <ckhosra1@yahoo.com>
2018-04-13 14:25:25 -0700

    DCP #9 Added admin structure

--- wakemeup/urls.py
+++ wakemeup/urls.py
@@ -1,11 +1,19 @@
 from django.conf.urls import url
 from . import views
 
-app_name= 'wakemeup' # qualifies url pattern names with 'itemdb' namespace (i.e. 'itemdb:iteminfo')
+app_name= 'wakemeup' # qualifies url pattern names with 'wakemeup' namespace (i.e. 'wakemeup:create_contract')
 urlpatterns = [
 
     # Index (i.e. /wakemeup)
     url(r'^$', views.index, name="index"),
+    
+    # Contract
     url(r'^contract/new$', views.create_contract, name="create_contract"),
-    url(r'^contract/new_new$', views.create_contract_new, name="create_contract_new"),
+    
+    # Admin
+    url(r'^admin/edit_school', views.add_school, name="edit_school"),
+    url(r'^admin/edit_class', views.add_school, name="edit_class"),
+    url(r'^admin/edit_teacher', views.add_school, name="edit_teacher"),
+    url(r'^admin/edit_student', views.add_school, name="edit_student"),
+    
 ]
