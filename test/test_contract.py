2a396e7b5f4d7903a576225246a7de4c3ff24569
Joe Smith <ckhosra1@yahoo.com>
2018-04-14 22:25:27 -0700

    Fixed incorrect reference.

--- test/test_contract.py
+++ test/test_contract.py
@@ -46,7 +46,7 @@ class testContracts(unittest.TestCase):
         )
         
         newteacher = Teacher(newuser.userid, classinfo)
-        newteacherid = newteacher.save_user()
+        newteacherid = newteacher.save()
 
     # Re-create environment for each test case        
     def setUp(self):
