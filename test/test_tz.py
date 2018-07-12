fc923f405bded4ea030da619f1999e4f77aa4f03
Joe Smith <ckhosra1@yahoo.com>
2018-07-11 21:20:20 -0500

    New utils functions.

--- test/test_tz.py
+++ test/test_tz.py
@@ -1,12 +1,12 @@
 import test_setup
-from UsefulFunctions.dateUtils import format_timestamp, format_timestamp_range_db
+from UsefulFunctions.dateUtils import format_timestamp_db, format_timestamp_range_db
 
 if(__name__) == "__main__":
 
     datetime_data = '2018-06-22'
     format1 = '%Y-%m-%d'
     
-    date_object = format_timestamp(datetime_data, format1, True)
+    date_object = format_timestamp_db(datetime_data, format1, True)
     
     print (date_object)
 
