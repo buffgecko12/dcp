f8c3957b9ec9a1ebe155f881d573273315c1d71f
Joe Smith <ckhosra1@yahoo.com>
2018-07-18 21:36:00 -0500

    Minor

--- wakemeup/models/contract.py
+++ wakemeup/models/contract.py
@@ -201,7 +201,7 @@ class Contract(models.Model):
     teacherrequirements = models.CharField(max_length=500)
     studentrequirements = models.CharField(max_length=500)
     contractscanfile = models.BinaryField()
-    contractapprovalts = models.DateTimeField(verbose_name='Fecha de aprobaci' + chr(243) + 'n')
+    contractapprovalts = models.DateTimeField()
     contractstatus = models.CharField(max_length=1,verbose_name='Estatus')
     goalinfo = JSONField()
     partyuserinfo = JSONField()
@@ -321,7 +321,7 @@ class Reward(models.Model):
     # Get party attributes
     rewardid = models.IntegerField(primary_key=True)
     rewarddisplayname = models.CharField(max_length=100,verbose_name='Premio')
-    rewarddescription = models.CharField(max_length=500,verbose_name='Descripci' + chr(243) + 'n')
+    rewarddescription = models.CharField()
     rewardvalue = models.IntegerField(verbose_name='Valor')
     createdbyuserid = models.IntegerField()
     
