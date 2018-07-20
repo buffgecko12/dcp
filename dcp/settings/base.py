b50ea6e5e6b1368c8b4e8e90358bdbace391c951
Joe Smith <ckhosra1@yahoo.com>
2018-07-19 19:33:53 -0500

    s

--- dcp/settings/base.py
+++ dcp/settings/base.py
@@ -81,7 +81,10 @@ DATABASE_URL = 'postgres://' + \
                 config('DB_HOST') + ':' + \
                 config('DB_PORT') + '/' + \
                 config('DB_NAME') + \
-                '?currentSchema=DCPViews'
+                '?currentSchema=' + config('DB_SCHEMA_NAME')
+
+DATABASE_URL='postgres://qwjabuvbukzyik:f61cacc5d4b419343352c8f5a885a98c28973f23ab3bc9cdded51c70662c4c0b@ec2-54-243-31-34.compute-1.amazonaws.com:5432/de6gbeq19knf3c?currentSchema=DCPViews'
+
 DATABASES['default'] = dj_database_url.config(default=DATABASE_URL,conn_max_age=600, ssl_require=True)
 
 # # Database
