4efef15cb61dea2e2a0a3069e3e92e9cc04613aa
Joe Smith <ckhosra1@yahoo.com>
2018-07-19 19:42:53 -0500

    s

--- dcp/settings/base.py
+++ dcp/settings/base.py
@@ -74,7 +74,15 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'dcp.wsgi.application'
 
-DATABASE_URL='FART'
+DATABASES = {}
+DATABASE_URL = 'postgres://' + \
+                config('DB_USER') + ':' + \
+                config('DB_PASSWORD') + '@' + \
+                config('DB_HOST') + ':' + \
+                config('DB_PORT') + '/' + \
+                config('DB_NAME') + \
+                '?currentSchema=' + config('DB_SCHEMA_NAME')
+DATABASES['default'] = dj_database_url.config(default=DATABASE_URL,conn_max_age=600, ssl_require=True)
 
 # # Database
 # DATABASES = {
