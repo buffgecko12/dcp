f378905f1d57c23aacaa9673413b3196046e4877
Joe Smith <ckhosra1@yahoo.com>
2018-07-21 06:51:20 -0500

    s

--- dcp/settings/prod.py
+++ dcp/settings/prod.py
@@ -9,3 +9,19 @@ print("BEFORE",ALLOWED_HOSTS)
 django_heroku.settings(locals())
 
 print("AFTER",ALLOWED_HOSTS)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
