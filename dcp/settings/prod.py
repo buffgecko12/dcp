99c1d40b123832a7b26408e2b5c2c55f6923f34c
Joe Smith <ckhosra1@yahoo.com>
2018-07-21 06:44:52 -0500

    s

--- dcp/settings/prod.py
+++ dcp/settings/prod.py
@@ -3,5 +3,9 @@ DEBUG = False
 
 ALLOWED_HOSTS += ['0.0.0.0', 'localhost', 'thawing-spire-81058.herokuapp.com']
 
+print("BEFORE",ALLOWED_HOSTS)
+
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+
+print("AFTER",ALLOWED_HOSTS)
