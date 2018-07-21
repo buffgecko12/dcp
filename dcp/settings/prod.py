2a475a330dfdce6fc63914b7088891270aabe15a
Joe Smith <ckhosra1@yahoo.com>
2018-07-21 06:31:30 -0500

    f

--- dcp/settings/prod.py
+++ dcp/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 DEBUG = False
 
-ALLOWED_HOSTS += ['*',]
+ALLOWED_HOSTS += ['thawing-spire-81058.herokuapp.com',]
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
