3164581b5c541b6c43f3fdb25aec23b6f0f19d61
Joe Smith <ckhosra1@yahoo.com>
2018-07-25 17:23:41 -0500

    .

--- dcp/settings/dev.py
+++ dcp/settings/dev.py
@@ -2,7 +2,7 @@ from .base import *
 from decouple import config
 
 DEBUG = True
-ALLOWED_HOSTS += ['localhost','127.0.0.1',]
+ALLOWED_HOSTS += ['localhost','127.0.0.1','192.168.0.3']
 
 INSTALLED_APPS += []
 MIDDLEWARE += []
