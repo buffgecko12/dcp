d2444dc61ec53ee6d407705b1b61d728d28a43d4
Joe Smith <ckhosra1@yahoo.com>
2018-07-31 23:37:37 -0500

    ff

--- dcp/settings/prod.py
+++ dcp/settings/prod.py
@@ -17,7 +17,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
         },
     },
 }
