15cf3e97a76a133692c70f9f8f857ca1a10a2fdb
Joe Smith <ckhosra1@yahoo.com>
2018-07-31 23:32:27 -0500

    d

--- dcp/settings/base.py
+++ dcp/settings/base.py
@@ -12,8 +12,8 @@ from lib.UsefulFunctions.envUtils import get_env_settings
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = config('SECRET_KEY')
 ALLOWED_HOSTS = []
