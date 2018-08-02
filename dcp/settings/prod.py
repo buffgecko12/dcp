f0f1887f8cc50adfd7fca9d98c5b75c82bbcd1b3
Joe Smith <ckhosra1@yahoo.com>
2018-08-01 19:32:15 -0500

    s

--- dcp/settings/prod.py
+++ dcp/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 import django_heroku
 
-DEBUG = True
+DEBUG = False
 
 # Configure Django App for Heroku (DATABASE_URL, ALLOWED_HOSTS, WhiteNoise Logging, Heroku CI)
 django_heroku.settings(locals())
