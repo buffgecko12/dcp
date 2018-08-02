b896ff52504e54b2e861af4e3a8bab4349cfc97f
Joe Smith <ckhosra1@yahoo.com>
2018-08-01 19:50:32 -0500

    work muthafucka

--- dcp/settings/base.py
+++ dcp/settings/base.py
@@ -108,7 +108,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/' # Url for static file serving
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # Search directories for static files (otherwise, engine only searches within app directories
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'dcp.storage.WhiteNoiseStaticFilesStorage'
 
 # Other settings
 LOGIN_REDIRECT_URL = '/' # Where to redirect login requests if "next" is not specified
