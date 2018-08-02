51d83bf262a3090ccd760e9f333ed5fc0790881b
Joe Smith <ckhosra1@yahoo.com>
2018-08-01 20:54:25 -0500

    DCP #13 More (e-mail)

--- dcp/settings/prod.py
+++ dcp/settings/prod.py
@@ -3,6 +3,13 @@ import django_heroku
 
 DEBUG = False
 
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
+
 # Configure Django App for Heroku (DATABASE_URL, ALLOWED_HOSTS, WhiteNoise Logging, Heroku CI)
 django_heroku.settings(locals())
 
