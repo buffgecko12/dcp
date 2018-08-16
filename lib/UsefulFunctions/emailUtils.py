eda37dcba4c545f6bef30514e3a114805bbce93c
Joe Smith <ckhosra1@yahoo.com>
2018-08-16 12:43:12 -0500

    DCP #30 Updated e-mail to send via BCC if multiple recipients

--- lib/UsefulFunctions/emailUtils.py
+++ lib/UsefulFunctions/emailUtils.py
@@ -1,9 +1,26 @@
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.conf import settings
 
-def send_email(subject, body, to_list, sender = None):
+def send_email(subject, body, to_list, bccflag = None, sender = None):
+
+    # Set bccflag = False (if not specified and only one person in to_list)
+    if(not bccflag and len(to_list) == 1):
+        bccflag = False
+    else:
+        bccflag = True
+    
     try:
         if(to_list):
-            send_mail(subject, body, sender, to_list)
+            msg = EmailMessage(
+                subject=subject, 
+                body=body, 
+                from_email=sender,
+                to=to_list if not bccflag else [settings.DEFAULT_FROM_EMAIL,],
+                bcc=to_list if bccflag else None
+            )
+        
+            msg.send()
+
     except Exception as e:
         print("ERROR - Could not send e-mail(s) - ", e)
 
