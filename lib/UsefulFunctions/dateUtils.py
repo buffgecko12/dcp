19e09293f3eaade3ec9c555479e0c647c576ba45
Joe Smith <ckhosra1@yahoo.com>
2018-08-22 19:40:42 -0500

    DCP #31 Added functions for "contract valid period" validation

--- lib/UsefulFunctions/dateUtils.py
+++ lib/UsefulFunctions/dateUtils.py
@@ -1,10 +1,26 @@
-import datetime
+from datetime import datetime
 from django.utils.timezone import get_current_timezone
-
 from psycopg2.extras import DateTimeTZRange
 
-def format_timestamp_db(timestamp, formatstring = '%d/%m/%Y', localizeFlag = True):
-    mytimestamp = datetime.datetime.strptime(timestamp, formatstring)
+def convert_date_to_timestamp_db(date, localizeFlag = True):
+    mytimestamp = datetime.combine(date, datetime.min.time())
+    
+    if(localizeFlag):
+        tz = get_current_timezone()
+        mytimestamp = tz.localize(mytimestamp)
+
+    return mytimestamp
+    
+# Functions to convert to native DB types
+def format_timestamp_range_db(starttimestamp, endtimestamp, formatstring = "%d/%m/%Y", localizeFlag = True, boundstring = '[]'):
+
+    mystarttimestamp = convert_date_to_timestamp_db(starttimestamp, localizeFlag)
+    myendtimestamp = convert_date_to_timestamp_db(endtimestamp, localizeFlag)
+    
+    return DateTimeTZRange(mystarttimestamp, myendtimestamp, boundstring) # Convert to Postgres native tstzrange type
+
+def format_timestamp_string_db(timestamp, formatstring = '%d/%m/%Y', localizeFlag = True):
+    mytimestamp = datetime.strptime(timestamp, formatstring)
     
     if(localizeFlag):
         tz = get_current_timezone()
@@ -12,13 +28,14 @@ def format_timestamp_db(timestamp, formatstring = '%d/%m/%Y', localizeFlag = Tru
 
     return mytimestamp
 
-def format_timestamp_range_db(timestamprange, formatstring, localizeFlag = True, boundstring = '[]'):
+def format_timestamp_range_string_db(timestamprange, formatstring, localizeFlag = True, boundstring = '[]'):
 
-    mystarttimestamp = format_timestamp_db(timestamprange[0], formatstring, localizeFlag)
-    myendtimestamp = format_timestamp_db(timestamprange[1], formatstring, localizeFlag)
+    mystarttimestamp = format_timestamp_string_db(timestamprange[0], formatstring, localizeFlag)
+    myendtimestamp = format_timestamp_string_db(timestamprange[1], formatstring, localizeFlag)
     
     return DateTimeTZRange(mystarttimestamp, myendtimestamp, boundstring) # Convert to Postgres native tstzrange type
 
+# Functions to format timestamps for display
 def display_timestamp(timestamp, formatstring = "%d/%m/%Y"):
 
     return str(timestamp.strftime(formatstring))
@@ -32,4 +49,4 @@ def display_timestamp_range(timestamprange, formatstring = "%d/%m/%Y"):
            str(timestamprange.upper.strftime(formatstring))
            
 def get_current_timestamp_db():
-    return format_timestamp_db(datetime.datetime.now())
+    return format_timestamp_string_db(datetime.datetime.now())
--- test/test_tz.py
+++ test/test_tz.py
@@ -1,19 +1,21 @@
 import test_setup
-from UsefulFunctions.dateUtils import format_timestamp_db, format_timestamp_range_db
+import unittest
+from UsefulFunctions.dateUtils import format_timestamp_string_db, format_timestamp_range_string_db, format_timestamp_range_db
+import datetime
 
-if(__name__) == "__main__":
-
-    datetime_data = '2018-06-22'
-    format1 = '%Y-%m-%d'
-    
-    date_object = format_timestamp_db(datetime_data, format1, True)
-    
-    print (date_object)
-
-    datetime_data = "['14/06/2018', '22/06/2018']"
-    
-    format1 = '%d/%m/%Y'
+class testTimestamp(unittest.TestCase):
+    def test_timestamp_functions(self):
+        # Timestamp string to db
+        datetime_data = '2018-06-22'
+        format1 = '%Y-%m-%d'
+        date_object = format_timestamp_string_db(timestamp = datetime_data, formatstring = format1)
         
-    date_object = format_timestamp_range_db(datetime_data, format1, True)
-    
-    print (date_object)
+        # Timestamp range string to db
+        datetime_data = ['14/06/2018', '22/06/2018']
+        format1 = '%d/%m/%Y'
+        date_object = format_timestamp_range_string_db(timestamprange=datetime_data, formatstring=format1)
+                
+        # Timestamp range to db
+        ts1 = datetime.datetime.now()
+        ts2 = ts1+ datetime.timedelta(days=1)
+        date_object = format_timestamp_range_db(starttimestamp = ts1, endtimestamp=ts2)
