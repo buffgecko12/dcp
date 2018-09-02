334f32a600868b4960d570cb0b09c02232a773d1
Joe Smith <ckhosra1@yahoo.com>
2018-09-02 10:57:06 -0500

    DCP #13 Updated initial data load

--- load_initial_data.py
+++ load_initial_data.py
@@ -6,8 +6,8 @@ from wakemeup.models.contract import Reward
 from lib.UsefulFunctions.stringUtils import mychr
 import json
 
-PASSWORD = 'adminadmin'
 MAX_BUDGET = 500000
+PASSWORD = "adminadmin"
 
 def normalize_field(mystring):
     return mystring.replace(" ","").lower()
@@ -32,11 +32,9 @@ def load_schools_classes():
     global ITIRR_SCHOOLID
 
     glv_classinfo = [
-        (7,'701'),(7,'702'),(7,'703'),(7,'704'),(7,'705'),(7,'706'),
-        (8,'801'),(8,'802'),(8,'809'),(8,'810'),(8,'811'),(8,'812'),
-        (9,'901'),(9,'902'),(9,'903'),(9,'904'),(9,'905'),(9,'906'),(9,'907'),(9,'908'),(9,'909'),(9,'910'),(9,'911'),(9,'912'),
-        (10,'1001'),(10,'1002'),(10,'1003'),(10,'1004'),(10,'1005'),(10,'1006'),(10,'1007'),(10,'1008'),(10,'1009'),(10,'1010'),(10,'1011'),
-        (11,'1101'),(11,'1102'),(11,'1103'),(11,'1104'),(11,'1105'),(11,'1106'),(11,'1107'),(11,'1108'),(11,'1109'),(11,'1110'),(11,'1111')
+        (7,'702'),(7,'704'),(7,'706'),
+        (10,'1002'),(10,'1006'),(10,'1007'),(10,'1008'),(10,'1009'),(10,'1010'),(10,'1011'),
+        (12,'1201'),
      ]
     
     rr_classinfo = [
@@ -45,7 +43,7 @@ def load_schools_classes():
         (8,'801'),(8,'802'),(8,'803'),(8,'804'),
         (9,'901'),(9,'902'),(9,'903'),(9,'904'),
         (10,'1001'),(10,'1002'),(10,'1003'),(10,'1004'),(10,'1005'),
-        (12,'1201') # Dummy data
+        (12,'1201'),
     ]
     
     school_list = [
@@ -78,55 +76,14 @@ def load_schools_classes():
 
 def load_users():
     admin_list = [
-        ('AD', None, 'Chris','Khosravi','S'),
-        ('AD', None, 'Assir','Sandoval','A'),
-        ('SF', GLV_SCHOOLID, 'Omaira-admin','Rincon','U'),
-        ('SF', GLV_SCHOOLID, 'Gladys-admin','Piracon','U'),
+        ('AD', None, 'admin','admin','S'),
     ]
 
     for admin in admin_list:
         create_user(admin[0], admin[1], admin[2], admin[3], admin[4])
 
     teacher_list = [        
-
-#         ('Suleima','Ferrer',GLV_SCHOOLID,['904','905','906'],),
-#         ('Alba Nelly','Salamanca',GLV_SCHOOLID,['901','902','903','1006','1007'],),
-#         ('Diana','Vera',GLV_SCHOOLID,['1004','1005','1101','1102','1103'],),
-#         ('Diego','Cruz',GLV_SCHOOLID,['701','702','703','704','705'],),
-#         ('Clara','Sanchez',GLV_SCHOOLID,['907','908','909','910'],),
-#         ('Adriana','Saenz',GLV_SCHOOLID,['1003','1108','1109','1110','1111'],),
-#         ('Liliana','Lizarazo',GLV_SCHOOLID,['1001','1104','1105','1106','1107'],),
-#         ('Lorena','Rojas',GLV_SCHOOLID,['1002','1008','1009','1010','1011'],),
-#         ('Doris','Avella',GLV_SCHOOLID,['706'],),
-#         ('Patricia','Conde',GLV_SCHOOLID,['911','912'],),
-#         
-        
-        ('Suleima','Ferrer',GLV_SCHOOLID,['904'],),
-        ('Alba Nelly','Salamanca',GLV_SCHOOLID,['901'],),
-        ('Diana','Vera',GLV_SCHOOLID,['1102'],),
-        ('Diego Fernando','Cruz',GLV_SCHOOLID,['701'],),
-        ('Clara','Sanchez',GLV_SCHOOLID,['907'],),
-        ('Adriana','Saenz',GLV_SCHOOLID,['1003'],),
-        ('Liliana','Lizarazo',GLV_SCHOOLID,['1001'],),
-        ('Lorena','Rojas',GLV_SCHOOLID,['1002'],),
-        ('Doris','Avella',GLV_SCHOOLID,['706'],),
-        ('Patricia','Conde',GLV_SCHOOLID,['911'],),
-        ('Zulma','Barreto',GLV_SCHOOLID,['812'],),
-        ('Tatiana','Chinome',GLV_SCHOOLID,['810'],),
-
-
-        ('Omaira','Rincon',GLV_SCHOOLID,['809'],),
-        ('Gladys','Piracon',GLV_SCHOOLID,['811'],),
-        ('David','TennisMachine',ITIRR_SCHOOLID,['1201'],),
-
-        
-        
-        ('Elizabeth','Moreno Garc' + mychr('i') + 'a',ITIRR_SCHOOLID,['601','602','603','604','605','801','802'],),
-        ('Sandra Patricia','Agudelo Acero',ITIRR_SCHOOLID,['701','702','703','704','705','803','804',],),
-        ('Diego','Mari' + mychr('n') + 'o Mart' + mychr('i') + 'nez',ITIRR_SCHOOLID,['1001','1002'],),
-        ('Jaime William','Corredor',ITIRR_SCHOOLID,['901','902','903','904','1003','1004','1005'],),
-        
-        
+         ('Chris','Khosravi',GLV_SCHOOLID,['1201'],),
     ]
 
     for teacher in teacher_list:
@@ -157,157 +114,14 @@ def load_users():
         myteacher.update_budget(maxbudget = MAX_BUDGET)
 
     student_list = [
-        
-        ('Jesus David','Martinez',GLV_SCHOOLID,'901'),
-        ('Juan Diego','Rodriguez',GLV_SCHOOLID,'901'),
-        ('Juan Felipe','Quatana Vargas',GLV_SCHOOLID,'901'),
-        ('Meslan Santiago','Puesta Cajica',GLV_SCHOOLID,'901'),
-
-        ('Daniel Esteban','Nino Mosquera',GLV_SCHOOLID,'904',),
-        ('Dianni Brithey','Hoyos Camacho',GLV_SCHOOLID,'904',),
-        ('Diego Andrey','Rueda Bayona',GLV_SCHOOLID,'904',),
-        ('Emerson Javier','Perez',GLV_SCHOOLID,'904',),
-        ('Jennifer Carolina','Galvis P',GLV_SCHOOLID,'904',),
-        ('Johan Santiago','Solano',GLV_SCHOOLID,'904',),
-#         ('Juan Diego','Bautista Corredor',GLV_SCHOOLID,'904',),
-
-        ('Brayan Camilo','Sanchez Arenas',GLV_SCHOOLID,'911'),
-        ('Jhony Esteban','Marchigo',GLV_SCHOOLID,'911'),
-        ('Julian','Ballesteros',GLV_SCHOOLID,'911'),
-        ('Karlheinz Alainthen','Moreno',GLV_SCHOOLID,'911'),
-
-        ('Fabian Camilo','Anaya',GLV_SCHOOLID,'1102'),
-        ('Valentina','Gaspar Baquero',GLV_SCHOOLID,'1102'),
-        ('Henry Felipe','Olarte Salgado',GLV_SCHOOLID,'1102'),
-        ('Lizeth Camila','Rivera Garcia',GLV_SCHOOLID,'1102'),
-        
-        ('Daniel Felipe','Triano',GLV_SCHOOLID,'907'),
-        ('Deisy Marcela','Fonseca',GLV_SCHOOLID,'907'),
-        ('Jerson David','Baquero',GLV_SCHOOLID,'907'),
-        ('Eduard Sebastian','Carvajal Fuentes',GLV_SCHOOLID,'907'),
-
-        ('Andrea Alexandra','Alvarez Manrique',GLV_SCHOOLID,'1001'),
-        ('Andrea Yuliana','Gonzalez',GLV_SCHOOLID,'1001'),
-        ('Nayelly Ximena','Rojas Torres',GLV_SCHOOLID,'1001'),
-        ('Jhonatan David','Fajardo',GLV_SCHOOLID,'1001'),
-
-        ('Aaron David','Quinonez',GLV_SCHOOLID,'1002'),
-        ('Juan Camilo','Ramos Ruiz',GLV_SCHOOLID,'1002'),
-        ('David ALejandro','Garcia Parra',GLV_SCHOOLID,'1002'),
-        ('Eliana Tamara','Bautista',GLV_SCHOOLID,'1002'),
-
-        ('Nancy Xiomara','Infante',GLV_SCHOOLID,'1003'),
-        ('Brajhan Jhair','Ravelo',GLV_SCHOOLID,'1003'),
-        ('Hugo Sebastian','Rojas Enciso',GLV_SCHOOLID,'1003'),
-        ('Johan Estiban','Mojica Sepulveda',GLV_SCHOOLID,'1003'),
-
-      
-#         ('Alejandra','Pita',GLV_SCHOOLID,'905'),
-        ('Claudio','Salamanca',GLV_SCHOOLID,'905'),
-        ('Dana','Camargo Castro',GLV_SCHOOLID,'905'),
-        ('Juan Sebastian','Corredor',GLV_SCHOOLID,'905'),
-        ('Katerin Daniela','Cucunuba',GLV_SCHOOLID,'905'),
-        ('Laura Sofia','Sandoval',GLV_SCHOOLID,'905'),
-        ('Natalia Valentina','Rosas Gomez',GLV_SCHOOLID,'905'),
-        ('Nathaly','Salazar',GLV_SCHOOLID,'905'),
-        ('Paula','Ramos',GLV_SCHOOLID,'905'),
-        
-        # Diego - 701 (really Alba, 906)
-        ('Cindy Valentina','Herrera Valderrama',GLV_SCHOOLID,'701'),
-        ('Julieth Xiomara','Gonzalez',GLV_SCHOOLID,'701'),
-        ('Juree Valentina','Gallo Santos',GLV_SCHOOLID,'701'),
-        ('Maria Fernanda','Duarte',GLV_SCHOOLID,'701'),
-
-        
-
-        ('Angie Lorena','Marquez Lopez',GLV_SCHOOLID,'902'),
-        ('Angie Natalia','Pinzon Sanabria',GLV_SCHOOLID,'902'),
-        ('Angy Viviana','Moreno Gonzalez',GLV_SCHOOLID,'902'),
-        ('Bresly Soledad','Sonabria',GLV_SCHOOLID,'902'),
-        ('Jennifer Alexandra','Coronado Pinzon',GLV_SCHOOLID,'902'),
-        
-        ('Ana Fernanda','Gomez Camargo',GLV_SCHOOLID,'903'),
-        ('Angela Johana','Acero Camargo',GLV_SCHOOLID,'903'),
-        ('Angela Katherina','Macias',GLV_SCHOOLID,'903'),
-        ('Angela Sofia','Perez',GLV_SCHOOLID,'903'),
-        ('Angie Jolieth','Mayarga Juarez',GLV_SCHOOLID,'903'),
-        
-        ('Yessid','Maldonado',GLV_SCHOOLID,'1006'),
-        ('Jose Alejandro','Pachecho Reyes',GLV_SCHOOLID,'1006'),
-        ('Sergio Alexander','Gonzalez',GLV_SCHOOLID,'1006'),
-        ('Luis Esteban','Benavides',GLV_SCHOOLID,'1006'),
-#        ('Valentina','Gomez Blanco',GLV_SCHOOLID,'1006'),
-
-        ('Andres Felipe','Chiuatu Parra',GLV_SCHOOLID,'1007'),
-        ('Dennys Aleyda','Vargas',GLV_SCHOOLID,'1007'),
-        ('Javier','Martinez Corredor',GLV_SCHOOLID,'1007'),
-        ('Jhonatan','Montanez Cespedes',GLV_SCHOOLID,'1007'),
-        ('Natalia Andrea','Cuervo Aranguren',GLV_SCHOOLID,'1007'),
-
-
-        ('Juan Esteban','Cuervo',GLV_SCHOOLID,'801'),
-        ('Sandra Milena','Pedraza',GLV_SCHOOLID,'801'),
-        ('Catalina','Becerra',GLV_SCHOOLID,'801'),
-        ('Laura Valentina','Parra',GLV_SCHOOLID,'801'),
-        
-        ('Farid','Martinez',GLV_SCHOOLID,'802'),
-        ('Dennys','Estupinan',GLV_SCHOOLID,'802'),
-        ('Leidy','Sandoval',GLV_SCHOOLID,'802'),
-
-        ('Laura Camila','Merchan',GLV_SCHOOLID,'809'),
-        ('Jorge Felipe','Rojas',GLV_SCHOOLID,'809'),
-        ('Jaider Eliot','Blanco',GLV_SCHOOLID,'809'),
-        ('Paula Alejandra','Garcia',GLV_SCHOOLID,'809'),
-
-        ('Maria Camila','Cuervo',GLV_SCHOOLID,'811'),
-        ('Brayan Esteban','Pardo',GLV_SCHOOLID,'811'),
-        ('Alfonso Diego','Avendona',GLV_SCHOOLID,'811'),
-        ('Luis Alejandro','Gonzalez',GLV_SCHOOLID,'811'),
-
-        ('Laura Maria','Supelano',GLV_SCHOOLID,'1101'),
-        ('Manuela','Bustamante',GLV_SCHOOLID,'1101'),
-        ('Omar','Nino',GLV_SCHOOLID,'1101'),
-        ('Karen Gisela','Sisa',GLV_SCHOOLID,'1103'),
-        ('Karol Tatiana','Guevara',GLV_SCHOOLID,'1103'),
-        ('Lizeth Paola','Martinez',GLV_SCHOOLID,'1103'),
-
-
-        
-        ('Yury','Galvan',ITIRR_SCHOOLID,'601'),
-        ('Danna Valentina','Pedraza',ITIRR_SCHOOLID,'601'),
-        ('Alejandra','Mora Cuspoca',ITIRR_SCHOOLID,'601'),
-        ('Mary Tatiana','Avendano',ITIRR_SCHOOLID,'601'),
-        
-        ('Brayan Estiven','Parra',ITIRR_SCHOOLID,'602'),
-        ('Brayan','Pardo',ITIRR_SCHOOLID,'602'),
-
-        ('Juan Filipe','Merchan',ITIRR_SCHOOLID,'801'),
-        ('Derly Yurany','Rojas',ITIRR_SCHOOLID,'801'),
-        ('Jaider Alejandro','Lopez',ITIRR_SCHOOLID,'801'),
-        ('Jorge Andres','Nino',ITIRR_SCHOOLID,'801'),
-
-        # Doris - 706
-        ('Angel Daniel','Camacho',GLV_SCHOOLID,'706'),
-        ('Dorlian Yizeth','Quinones',GLV_SCHOOLID,'706'),
-        ('Manuel Fernando','Vargas',GLV_SCHOOLID,'706'),
-        ('Yimi Alexander','Pamplona',GLV_SCHOOLID,'706'),
-        
-        # Tatiana - 810
-        ('Milton','Camacho',GLV_SCHOOLID,'810'),
-        ('Angel David','Quinones',GLV_SCHOOLID,'810'),
-        ('Jose Miguel','Vargas',GLV_SCHOOLID,'810'),
-        ('Juan Jose','Pamplona',GLV_SCHOOLID,'810'),
-        
-        # Zulma - 812
-        ('Edgar','Camacho',GLV_SCHOOLID,'812'),
-        ('Samuel','Quinones',GLV_SCHOOLID,'812'),
-        ('Carlos','Vargas',GLV_SCHOOLID,'812'),
-        ('Juan Miguel','Pamplona',GLV_SCHOOLID,'812'),
-
-        ('Roger','Federer',ITIRR_SCHOOLID,'1201'),
-        ('Novak','Djokovic',ITIRR_SCHOOLID,'1201'),
-        ('Rafael','Nadal',ITIRR_SCHOOLID,'1201'),
-        ('Stefan','Edberg',ITIRR_SCHOOLID,'1201'),        
+        ('Roger','Federer',ITIRR_SCHOOLID,'1201'),        
+        ('Rafael','Nadal',ITIRR_SCHOOLID,'1201'),        
+        ('Steffi','Graf',ITIRR_SCHOOLID,'1201'),        
+        ('Angelique','Kerber',ITIRR_SCHOOLID,'1201'),        
+        ('Roberto','Clemente',GLV_SCHOOLID,'1201'),        
+        ('Hank','Aaron',GLV_SCHOOLID,'1201'),        
+        ('Nelson','Mandela',GLV_SCHOOLID,'1201'),        
+        ('Jackie','Robinson',GLV_SCHOOLID,'1201'),        
     ]
 
     for student in student_list:
