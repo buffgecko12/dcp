6435d40a4b0db764227bfda5ec889f5bdd35e402
Chris Khosravi <chris.khosravi@gmail.com>
2020-02-13 21:19:13 +0200

    DCP #1 Added Contract Party Reward

--- test/test_contract.py
+++ test/test_contract.py
@@ -26,6 +26,9 @@ class testContracts(unittest.TestCase):
 
         # Create contract file
         cls.myfile = create_file(filename='contractscan',fileextension='pdf',filetype='application/pdf',filedescription='Class 1 - Contract',filestore='DB',filecategory='CT')
+
+        cls.myreward1 = create_reward(rewarddisplayname='My Reward 1')
+        cls.myreward2 = create_reward(rewarddisplayname='My Reward 2',rewardvalue=20000)
         
     # Re-create environment for each test case        
     def setUp(self):
@@ -42,7 +45,8 @@ class testContracts(unittest.TestCase):
 
         # Contract parties / reward
         self.mycontractparty1 = create_contract_party(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher2.userid,classid=self.myclass3.classid)
-#         self.mycontractpartyreward = create_contract_party_reward()
+        self.mycontractpartyreward1 = create_contract_party_reward(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher1.userid,classid=self.myclass1.classid,rewardid=self.myreward1.rewardid)
+        self.mycontractpartyreward2 = create_contract_party_reward(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher2.userid,classid=self.myclass3.classid,rewardid=self.myreward2.rewardid,quantity=10)
 
         # Associate file with contract 1
         self.myfile.contractid = self.mycontract1.contractid
@@ -53,7 +57,7 @@ class testContracts(unittest.TestCase):
         self.assertTrue(self.mycontract1.contractid)
 
     def testGetContract(self):
-        self.assertTrue(Contract.objects.get(contractid=self.mycontract1.contractid))
+        self.assertIsNotNone(Contract.objects.get(contractid=self.mycontract1.contractid))
         self.assertEqual(len(Contract.objects.get_contracts(schoolyear=2000)),1) # specific school year
         self.assertEqual(len(Contract.objects.get_contracts(schoolyear=DEFAULT_SCHOOL_YEAR)),1) # default school year
         self.assertEqual(len(Contract.objects.get_contracts(teacheruserid=self.myteacher1.userid)),2) # teacher (positive) - 2 contracts
@@ -72,9 +76,8 @@ class testContracts(unittest.TestCase):
         pass
     
     def testGetContractParty(self):
-        
         self.assertIsNotNone(ContractParty.objects.all()) # all parties
-        self.assertTrue(ContractParty.objects.get(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher1.userid,classid=self.myclass1.classid)) # specific party
+        self.assertIsNotNone(ContractParty.objects.get(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher1.userid,classid=self.myclass1.classid)) # specific party
         self.assertEqual(len(ContractParty.objects.get_contract_parties(contractid=self.mycontract1.contractid)),3) # a specific contract's parties (3)
         self.assertEqual(len(ContractParty.objects.get_contract_parties(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher2.userid)),2) # a teacher's parties (2) for a specific contract
 
@@ -93,6 +96,7 @@ class testContracts(unittest.TestCase):
         self.assertIsNone(self.mycontractparty1.numparticipants)
 
     def testdeleteContractParty(self):
+        
         # Check parties exist
         self.assertTrue(ContractParty.objects.get_contract_parties(teacheruserid=self.myteacher2.userid))
 
@@ -104,8 +108,30 @@ class testContracts(unittest.TestCase):
         self.assertFalse(ContractParty.objects.get_contract_parties(teacheruserid=self.myteacher2.userid))
         self.assertTrue(ContractParty.objects.get_contract_parties(teacheruserid=self.myteacher1.userid))
 
+    def testCreateContractPartyReward(self):
+        pass
+    
+    def testGetContractPartyReward(self):
+        self.assertIsNotNone(ContractPartyReward.objects.all())
+        self.assertIsNotNone(ContractPartyReward.objects.get(contractid=self.mycontract1.contractid,teacheruserid=self.myteacher1.userid,classid=self.myclass1.classid,rewardid=self.myreward1.rewardid))
+        self.assertTrue(len(ContractPartyReward.objects.get_contract_party_rewards(contractid=self.mycontract1.contractid)),2) # check for contract rewards 
+        self.assertTrue(len(ContractPartyReward.objects.get_contract_party_rewards(classid=self.myclass3.classid)),1) # check for class's total rewards 
+
+    def testDeleteContractPartyReward(self):
+        
+        # Check party rewards exist
+        self.assertTrue(ContractPartyReward.objects.get_contract_party_rewards(teacheruserid=self.myteacher1.userid))
+
+        # Delete parties        
+        for mycontractpartyreward in ContractPartyReward.objects.get_contract_party_rewards(teacheruserid=self.myteacher1.userid):
+            mycontractpartyreward.delete()
+            
+        # Check appropriate parties deleted
+        self.assertFalse(ContractPartyReward.objects.get_contract_party_rewards(teacheruserid=self.myteacher1.userid))
+        self.assertTrue(ContractPartyReward.objects.get_contract_party_rewards(teacheruserid=self.myteacher2.userid))
+
     def tearDown(self):
-        self.mycontract1.delete() # Deletes any related contract info (i.e. parties)
+        self.mycontract1.delete() # Deletes any related contract info (i.e. parties / rewards)
         self.mycontract2.delete()
 
     @classmethod
