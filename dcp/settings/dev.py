c165ffdb98229bdd4a8925c7c710ebf05cf5f7dc
Chris Khosravi <chris.khosravi@gmail.com>
2020-02-28 20:37:02 +0200

    ssl testing

--- dcp/settings/dev.py
+++ dcp/settings/dev.py
@@ -4,6 +4,6 @@ from decouple import config
 DEBUG = True
 ALLOWED_HOSTS += [
     'localhost','127.0.0.1','192.168.0.2','192.168.0.3', # development
-    'dcp2-tst.herokuapp.com','dev.duitamacolegioproject.org', # test
+    'dcp2-tst.herokuapp.com','tst.duitamacolegioproject.org', # test
     'dcp2-stg.herokuapp.com','stg.duitamacolegioproject.org' # staging
     ]
