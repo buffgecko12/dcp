c362333570c02017b37040c847ed9db8235f0db9
Chris Khosravi <chris.khosravi@gmail.com>
2020-03-14 10:04:22 +0200

    Updating localhost IP for dev.

--- dcp/settings/dev.py
+++ dcp/settings/dev.py
@@ -2,6 +2,6 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS += [
-    'localhost','127.0.0.1','192.168.0.2','192.168.0.3', # development
+    'localhost','127.0.0.1','10.0.0.1', # development
     'dcp2-tst.herokuapp.com','tst.duitamacolegioproject.org', # test
     ]
