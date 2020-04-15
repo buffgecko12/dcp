d590690d6ca6558141b9ab96021fd22ec103f35e
Chris Khosravi <chris.khosravi@gmail.com>
2020-04-15 06:48:21 +0300

    DCP #5 - Testing HttpResponse

--- lib/UsefulFunctions/httpUtils.py
+++ lib/UsefulFunctions/httpUtils.py
@@ -1,7 +1,8 @@
-from django.http import FileResponse
+from wsgiref.util import FileWrapper
+from django.http import FileResponse, HttpResponse
 
 def getFileResponse(filedata, filename, filesize, contenttype):
-    response = FileResponse(filedata, content_type=contenttype) # Can also use write() function to create new "file"
+    response = HttpResponse(filedata, content_type=contenttype) # Can also use write() function to create new "file"
     response['Content-Disposition'] = 'attachment; filename=%s' % filename # force browser to download file
     response['Content-Length'] = filesize
     return response
