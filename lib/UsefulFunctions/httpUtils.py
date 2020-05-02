df9cdeb17b1f9725419ba96d18f6519dc638022c
Chris Khosravi <chris.khosravi@gmail.com>
2020-05-02 05:37:34 +0300

    DCP #5 Changed default FileResponse to regular

--- lib/UsefulFunctions/httpUtils.py
+++ lib/UsefulFunctions/httpUtils.py
@@ -1,8 +1,8 @@
 from wsgiref.util import FileWrapper
 from django.http import FileResponse, HttpResponse
 
-def getFileResponse(filedata, filename, filesize, contenttype):
+def getFileResponse(filedata, filename, filesize, contenttype, forcedownload=False):
     response = HttpResponse(filedata, content_type=contenttype) # Can also use write() function to create new "file"
-    response['Content-Disposition'] = 'attachment; filename=%s' % filename # force browser to download file
+    response['Content-Disposition'] = '%s; filename=%s' % ('attachment' if forcedownload else '', filename) # force browser to download file
     response['Content-Length'] = filesize
     return response
