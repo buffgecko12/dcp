b539a399cc883d7f43ae3a4cc8738dc0e87e2076
Chris Khosravi <chris.khosravi@gmail.com>
2025-02-17 07:04:21 -0800

    DCP-71 Adding IP for testing

--- dcp/settings/dev.py
+++ dcp/settings/dev.py
@@ -2,5 +2,5 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS += [
-    'localhost','127.0.0.1','10.0.0.1' # development
+    'localhost','127.0.0.1','10.0.0.1','192.168.88.253' # development
     ]
